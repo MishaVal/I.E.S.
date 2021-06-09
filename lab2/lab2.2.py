42cb0f273c7a2b50f0230743cbef1e348a0b5702
MishaVal <50676904+MishaVal@users.noreply.github.com>
2021-06-09 08:45:41 +0300

    lab2.2 dop

--- lab2/lab2.2.py
+++ lab2/lab2.2.py
@@ -6,6 +6,16 @@ HARMONICS = 8
 FREQUENCY = 1100
 DISCRETE_CALLS = 256
 
+def calculate_time(signal_harmonics, start_frequency, number_discrete_calls):
+    time = []
+    for n in range(number_discrete_calls):
+    	signal = signal_generator.generate_signal(signal_harmonics, start_frequency, n)
+        start_time = t.time()
+        fft.fast_fourier_transform(signal)
+        end_time = t.time()
+        time.append(end_time - start_time)
+    return time
+
 signal = signal_generator.generate_signal(
   HARMONICS,
   FREQUENCY,
@@ -13,7 +23,14 @@ signal = signal_generator.generate_signal(
 )
 spectrum_fft = fft.fast_fourier_transform(signal)
 print(spectrum_fft)
-fig, (ax1, ax2) = plt.subplots(2)
+
+time = complexity_generator.calculate_time(
+  HARMONICS,
+  FREQUENCY,
+  2500
+)
+
+fig, (ax1, ax2, ax3) = plt.subplots(3)
 fig.suptitle('Laboratorka 2.2')
 fig.set_size_inches(18.5, 10.5)
 
@@ -26,4 +43,8 @@ ax2.set_title('Fast Fourier transform of generated signal')
 ax2.set(xlabel='time', ylabel='fft')
 fig.savefig('lab2_2.png')
 
+ax2.plot(time)
+ax2.set_title('FFT time dependency')
+ax2.set(xlabel='N', ylabel='time')
+fig.savefig('lab2_2.png')
 plt.show()
